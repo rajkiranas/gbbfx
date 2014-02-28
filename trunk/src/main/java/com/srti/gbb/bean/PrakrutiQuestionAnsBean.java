0b81964bd2955d4b2f289127535caabe4c246e54
rajkiranas@gmail.com <rajkiranas@gmail.com@f289ffbf-dc74-f42e-0bd8-e227f8f6591c>
2014-02-28 08:41:07 +0000

    recording user responses for prakruti questions , with validations

---
+++ trunk/src/main/java/com/srti/gbb/bean/PrakrutiQuestionAnsBean.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.srti.gbb.bean;
+
+/**
+ *
+ * @author rajkirans
+ */
+public class PrakrutiQuestionAnsBean 
+{
+    private String question;
+    private String ans1;
+    private String ans2;
+    private String ans3;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAns1() {
+        return ans1;
+    }
+
+    public void setAns1(String ans1) {
+        this.ans1 = ans1;
+    }
+
+    public String getAns2() {
+        return ans2;
+    }
+
+    public void setAns2(String ans2) {
+        this.ans2 = ans2;
+    }
+
+    public String getAns3() {
+        return ans3;
+    }
+
+    public void setAns3(String ans3) {
+        this.ans3 = ans3;
+    }
+    
+        
+}
