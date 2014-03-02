fd9ca164c1aa3ac59f61a94f5b8873b298d491ec
rajkiranas@gmail.com <rajkiranas@gmail.com@f289ffbf-dc74-f42e-0bd8-e227f8f6591c>
2014-03-02 04:38:26 +0000

    cleanup in validations and navigation

--- trunk/src/main/java/com/srti/gbb/main/ScreensFramework.java
+++ trunk/src/main/java/com/srti/gbb/main/ScreensFramework.java
@@ -76,7 +76,7 @@ public class ScreensFramework extends Application {
         nav.loadScreen(ScreensFramework.ThankyouSceneId, ScreensFramework.ThankyouSceneFile);
         
         
-        nav.navigateTo(ScreensFramework.screen10ID);
+        nav.navigateTo(ScreensFramework.screen0ID);
         
         Group root = new Group();
         root.getChildren().addAll(nav);
