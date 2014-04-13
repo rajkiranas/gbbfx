0de4b9c4ab42324aebb60024f6290bde6f5dc059
rajkiranas@gmail.com <rajkiranas@gmail.com@f289ffbf-dc74-f42e-0bd8-e227f8f6591c>
2014-04-13 08:02:18 +0000

    soupse and child screen functional - qa remaining fridnds layout changed

--- trunk/src/main/java/com/srti/gbb/controller/Screen18Controller.java
+++ trunk/src/main/java/com/srti/gbb/controller/Screen18Controller.java
@@ -11,12 +11,16 @@ import com.srti.gbb.navigator.ScreensNavigator;
 import com.srti.gbb.utils.MU;
 import com.srti.gbb.utils.UIUtils;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
 
 /**
@@ -27,6 +31,10 @@ import javafx.scene.control.TextField;
 public class Screen18Controller implements  Initializable, ControlledScreen {
 
     private ScreensNavigator navigator;
+    
+    private List<FamilyEducation> childrenList = new ArrayList<FamilyEducation>();
+    
+    
     
     
     /**
@@ -39,6 +47,7 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
         //populateProfession();
         populateIncome();
         populateChildGender();
+        hideShowPreviousLink();
     }
     
      @Override
@@ -55,10 +64,33 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
     @FXML
     private void goToNextScreen(ActionEvent event) 
     {
-        if(validateSpouseInformation())
+        if(cmbChildGender.getValue()!=null || 
+                cmbChildQual.getValue()!=null || 
+                //cmbFMOccupation.getValue()!=null || 
+                    !txtChildName.getText().equals(GlobalConstants.emptyString) ||
+                cmbChildProfession.getValue()!=null || 
+                cmbChildIncome.getValue()!=null ||
+                    chkIsChildDeceased.isSelected())
+            {
+                if(validateForFamilyMemberForm())
+                {
+                    addToFamilyMembersList(getFamilyMemberBeanForCurrentForm());
+                    clearFamilyMemberForm();
+                    showPreviousLink();
+                }
+            }
+        
+        if((childrenList.size()==navigator.getUserInfo().getPi().getNoOfChildren() || childrenList.size()>navigator.getUserInfo().getPi().getNoOfChildren())
+                && validateSpouseInformation())
         {
+            setFamilyEducationData();
             setSpouseData();
             navigateToNextScreen();
+            
+        }
+        else
+        {
+            UIUtils.showAlert("sc18_msg_enter_correct_child_no", GlobalConstants.Lbl_Alert);
         }
     }
     
@@ -85,23 +117,7 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
     
     
     
-    @FXML
-    private TextField txtChildName;
     
-    @FXML
-    private ComboBox cmbChildGender;
-    
-    @FXML
-    private ComboBox cmbChildQual;
-    
-    @FXML
-    private ComboBox cmbChildProfession;
-    
-    @FXML
-    private ComboBox cmbChildIncome;
-    
-    @FXML
-    private CheckBox chkIsChildDeceased;
     
     private boolean validateSpouseInformation() 
     {
@@ -146,6 +162,7 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
         m.setProfession(cmbSpouseProfession.getValue().toString());
         m.setIncome(cmbSpouseIncome.getValue().toString());
         m.setIsDeceased(chkIsSpouseDeceased.isSelected());
+        System.out.println("****Spouse="+m);
         navigator.getUserInfo().setSpouse(m);
     }
 
@@ -176,17 +193,18 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
         String genderList = GlobalConstants.getProperty(GlobalConstants.Profession_Options);
         String[] list =  genderList.split(GlobalConstants.COMMA);
         
+        // this is opposite case - male profile will fill up form for his spouse
+        //and female profile will fillup for her spouse
         if(navigator.getUserInfo().getPi().getGender().equals(MU.getMsg("Lbl_Male")))
         {
             if(cmbSpouseProfession.getItems().size()==0)
             {
                 for(String gen : list)
                 {
-                    if(!gen.equals(MU.getMsg("Lbl_Housewife")))
+                    //if(!gen.equals(MU.getMsg("Lbl_Housewife")))
                         cmbSpouseProfession.getItems().addAll(gen);                    
                 }
             }
-            
         }
         else
         {
@@ -194,7 +212,7 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
             {
                 for(String gen : list)
                 {
-                    //if(!gen.equals(MU.getMsg("Lbl_Housewife")))
+                    if(!gen.equals(MU.getMsg("Lbl_Housewife")))
                         cmbSpouseProfession.getItems().addAll(gen);                    
                 }
             }
@@ -214,7 +232,6 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
             }
         }
         
-        
         if(cmbChildIncome.getItems().size()==0)
         {
             for(String gen : list)
@@ -222,8 +239,6 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
                     cmbChildIncome.getItems().addAll(gen);                    
             }
         }
-        
-        
     }
     
     private void populateChildGender() {
@@ -272,5 +287,167 @@ public class Screen18Controller implements  Initializable, ControlledScreen {
             }
             
         }
+    }
+    
+     @FXML
+    Hyperlink linkShowPrevious;
+     
+     @FXML
+    private TextField txtChildName;
+    
+    @FXML
+    private ComboBox cmbChildGender;
+    
+    @FXML
+    private ComboBox cmbChildQual;
+    
+    @FXML
+    private ComboBox cmbChildProfession;
+    
+    @FXML
+    private ComboBox cmbChildIncome;
+    
+    @FXML
+    private CheckBox chkIsChildDeceased;
+    
+    @FXML
+    private Button btnAddFM;
+    
+    
+    private void hideShowPreviousLink() {
+        linkShowPrevious.setVisible(false);
+    }
+    
+    private void showPreviousLink() {
+        linkShowPrevious.setVisible(true);
+    }
+ 
+    private int previousCounter=0;
+    
+     @FXML
+    private void showPrevious()
+    {
+        if(previousCounter==0)
+        {
+            previousCounter=childrenList.size()-1;
+        }
+        else
+        {
+            previousCounter--;        
+        }
+            
+        FamilyEducation member = childrenList.get(previousCounter);
+        
+        String m = member.getFamilyMember();
+        String fQual = member.getHighestQualification();
+        String fProfession = member.getProfession();
+        String fIncome = member.getIncome();            
+        
+        if(m.equals(MU.getMsg("Lbl_Son")))   
+        {
+           cmbChildGender.getSelectionModel().select(MU.getMsg("Lbl_Male"));
+        }
+        else
+        {
+            cmbChildGender.getSelectionModel().select(MU.getMsg("Lbl_Female"));
+        }
+        cmbChildQual.getSelectionModel().select(fQual);
+        txtChildName.setText(member.getName());
+        cmbChildProfession.getSelectionModel().select(fProfession);
+        cmbChildIncome.getSelectionModel().select(fIncome);        
+        chkIsChildDeceased.setSelected(member.isIsDeceased());
+    }
+    
+    @FXML
+    private void addFamilyMember(ActionEvent event) 
+    {
+        if(validateForFamilyMemberForm())
+        {
+            addToFamilyMembersList(getFamilyMemberBeanForCurrentForm());
+            
+            clearFamilyMemberForm();
+            showPreviousLink();
+        }
+    }
+    
+    private FamilyEducation getFamilyMemberBeanForCurrentForm()
+    {
+        FamilyEducation bean = new FamilyEducation();
+        
+        if(cmbChildGender.getValue().toString().equals(MU.getMsg("Lbl_Male")))
+        {
+            bean.setFamilyMember(MU.getMsg("Lbl_Son"));
+        }
+        else
+        {
+            bean.setFamilyMember(MU.getMsg("Lbl_Daughter"));
+        }
+        
+        bean.setHighestQualification(cmbChildQual.getValue().toString());
+        bean.setName(txtChildName.getText());
+        bean.setProfession(cmbChildProfession.getValue().toString());
+        bean.setIncome(cmbChildIncome.getValue().toString());
+        bean.setIsDeceased(chkIsChildDeceased.isSelected());
+        return bean;
+    }
+    private void clearFamilyMemberForm()
+    {
+        cmbChildGender.getSelectionModel().clearSelection();
+        
+        cmbChildQual.getSelectionModel().clearSelection();
+        txtChildName.setText(GlobalConstants.emptyString);
+        cmbChildProfession.getSelectionModel().clearSelection();
+        cmbChildIncome.getSelectionModel().clearSelection();
+        chkIsChildDeceased.setSelected(false);
+    }
+    
+    private void addToFamilyMembersList(FamilyEducation familyMemberBeanForCurrentForm) 
+    {
+        if(childrenList.contains(familyMemberBeanForCurrentForm))
+        {
+            childrenList.remove(familyMemberBeanForCurrentForm);
+            childrenList.add(familyMemberBeanForCurrentForm);
+        }
+        else
+        {
+            childrenList.add(familyMemberBeanForCurrentForm);
+        }
+    }
+    
+    private boolean validateForFamilyMemberForm() 
+    {
+        boolean isValid=true;
+        
+        if(txtChildName.getText()==null || txtChildName.getText().equals(GlobalConstants.emptyString))
+        {
+            isValid=false;
+            UIUtils.showAlert("sc18_msg_enter_child_name", GlobalConstants.Lbl_Alert);
+        }
+        else if(cmbChildGender.getValue()==null || cmbChildGender.getValue().toString().equals(GlobalConstants.emptyString))
+        {
+            isValid=false;
+            UIUtils.showAlert("sc18_msg_sel_child_gender", GlobalConstants.Lbl_Alert);
+        } 
+        else if(cmbChildQual.getValue()==null || cmbChildQual.getValue().toString().equals(GlobalConstants.emptyString))
+        {
+            isValid=false;
+            UIUtils.showAlert("sc18_msg_sel_child_qual", GlobalConstants.Lbl_Alert);
+        }
+        else if(cmbChildProfession.getValue()==null || cmbChildProfession.getValue().toString().equals(GlobalConstants.emptyString))
+        {
+            isValid=false;
+            UIUtils.showAlert("sc18_msg_sel_child_prof", GlobalConstants.Lbl_Alert);
+        }
+        else if(cmbChildIncome.getValue()==null || cmbChildIncome.getValue().toString().equals(GlobalConstants.emptyString))
+        {
+            isValid=false;
+            UIUtils.showAlert("sc18_msg_sel_child_income", GlobalConstants.Lbl_Alert);
+        }
+        return isValid;
+    }
+
+    private void setFamilyEducationData() {
+        System.out.println("***childrenList="+childrenList);
+        navigator.getUserInfo().setChildrenList(childrenList);
     }
 }
