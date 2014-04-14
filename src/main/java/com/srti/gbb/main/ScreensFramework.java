d064f8a0048549d77c682adb1c2f5eebe9667e00
rajkiranas@gmail.com <rajkiranas@gmail.com@f289ffbf-dc74-f42e-0bd8-e227f8f6591c>
2014-04-14 06:31:17 +0000

    ui cleanup

--- src/main/java/com/srti/gbb/main/ScreensFramework.java
+++ src/main/java/com/srti/gbb/main/ScreensFramework.java
@@ -110,7 +110,7 @@ public class ScreensFramework extends Application {
         nav.loadScreen(ScreensFramework.ThankyouSceneId, ScreensFramework.ThankyouSceneFile);
         
         
-        nav.navigateTo(ScreensFramework.screen19ID);
+        nav.navigateTo(ScreensFramework.screen0ID);
         
         Group root = new Group();
         root.getChildren().addAll(nav);
